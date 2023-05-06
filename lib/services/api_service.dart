48de0c6af747c78af76ce8974b4866f937144408
jayhawk <jayhawk.dev@gmail.com>
2023-05-07 05:09:32 +0900

    Feat: 오늘의 웹툰 API 연동
    http package 추가 및 API로 json파일 받아와서 decoding

---
+++ lib/services/api_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    //비동기 프로그래밍을 위해서 사용
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url); //async(비동기)에서만 await 사용 가능
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
